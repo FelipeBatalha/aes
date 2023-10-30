fd49a8bb4c6769f4b9464add8d7e028ee6f48ddc
Felipe Batalha <felipe.batalha@ish.com.br>
2023-10-30 09:01:15 -0300

    user interaction improvements

--- ctr-mode.py
+++ ctr-mode.py
@@ -1,17 +1,15 @@
 import numpy as np
 from encrypt import string_to_hex, aes_encryption
-from decrypt import aes_decryption
 
 input_file = 'ah-pronto.png'
 output_file = 'encrypted.bin'
 
 
-def encrypt():
+def encrypt(rounds):
     iv = b"myKingBu"
     key = string_to_hex("Thats my Kung Fu").transpose()
     with open(input_file, 'rb') as file_in, open(output_file, 'wb') as file_out:
         counter = 0
-        #file_out.write(bytes.fromhex('89504e470d0a1a0a0000'))
         file_out.write(iv)
         while True:
             print(counter)
@@ -19,21 +17,16 @@ def encrypt():
             if len(data) == 0:
                 break
 
-            #data = string_to_hex(data).transpose()
             nonce = iv + counter.to_bytes(8, byteorder='big')  # Create the unique nonce
-            keystream = aes_encryption(10, string_to_hex(nonce).transpose(), key)
-            
+            keystream = aes_encryption(rounds, string_to_hex(nonce).transpose(), key)
             ciphertext = bytes(x ^ y for x, y in zip(keystream, data))
-
             file_out.write(ciphertext)
             counter += 1
 
-def decrypt():
-    #iv = b"myKingBu"
+def decrypt(rounds):
     key = string_to_hex("Thats my Kung Fu").transpose()
     with open('encrypted.bin', 'rb') as file_in, open('decrypted.png', 'wb') as file_out:
         counter = 0
-        #file_in.read(10)
         iv = file_in.read(8)
         print(iv)
         while True:
@@ -42,14 +35,29 @@ def decrypt():
                 break
             print(counter)
 
-            nonce = iv + counter.to_bytes(8, byteorder='big')  # Create the unique nonce
-            keystream = aes_encryption(10, string_to_hex(nonce).transpose(), key)
-            #plaintext = aes_decryption(10, string_to_hex(keystream).transpose(), key)
+            nonce = iv + counter.to_bytes(8, byteorder='big')
+            keystream = aes_encryption(rounds, string_to_hex(nonce).transpose(), key)
             plaintext = bytes(x ^ y for x, y in zip(keystream,data))
-
             file_out.write(plaintext)
             counter += 1
 
 if __name__ == "__main__":
-     encrypt()
-     decrypt()
+     
+    while True:
+        print("Escolha uma opção:")
+        print("1. Cifrar")
+        print("2. Decifrar")
+        print("3. Sair")
+
+        choice = input("Escolha: ")
+
+        if choice == "1":
+            rounds = int(input("Número de rounds: "))
+            encrypt(rounds)
+        elif choice == "2":
+            rounds = int(input("Número de rounds: "))
+            decrypt(rounds)
+        elif choice == "3":
+            break
+        else:
+            print("Escolha inválida. Selecione 1, 2, or 3.")
